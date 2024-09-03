9e977881ee90f8c99ce2d06a2828bc7c6ba34206
k.demchenko <k.demchenko@cfunalog.ru>
2024-09-03 17:34:52 +0300

    comment ai task

--- source/main.py
+++ source/main.py
@@ -1,9 +1,9 @@
 from asyncio import run
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
-from deep_translator import GoogleTranslator
+# from deep_translator import GoogleTranslator
 from tortoise import run_async
 from init import init_conn, start_sheduler, init_db
-from tasks.ai_bot import get_ai_answers
+# from tasks.ai_bot import get_ai_answers
 from tasks.api import send_reward_to_leaders
 
 
@@ -12,14 +12,14 @@ async def main():
     Функция запуска планировщика
     """
     # Инициализируем соединения с Redis, Google, OpenAI
-    conn = await init_conn()  # Redis подключения
-    gt = GoogleTranslator(source='en', target='ru')  # Google
+    # conn = await init_conn()  # Redis подключения
+    # gt = GoogleTranslator(source='en', target='ru')  # Google
 
     # Инициализируем шедулер
     scheduler = AsyncIOScheduler()
 
     # Подключаем таски
-    scheduler.add_job(get_ai_answers, trigger="interval", seconds=5, kwargs={'conn': conn, "gt": gt})
+    # scheduler.add_job(get_ai_answers, trigger="interval", seconds=5, kwargs={'conn': conn, "gt": gt})
     scheduler.add_job(send_reward_to_leaders, trigger="cron", day_of_week=6, hour=3, minute=0)
 
     # Запускаем таск менеджер
